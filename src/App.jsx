5e10beb8820ab67a1a83d1ddd12854dd2acdd954
Israel Sánchez <israelclarinete@gmail.com>
2024-06-29 18:12:58 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {} from "react";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-3xl uppercase bg-green-400 text-center p-4">
+        Store Tickets Safety
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
